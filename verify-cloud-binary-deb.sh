65fccc9caebea84b959bafa5e4c4f6adf47809ef
Teus Benschop <teusjannette@gmail.com>
2026-06-14 12:01:23 +0200

    Script to check binary .deb on quality
    https://github.com/bibledit/cloud/issues/1095

---
+++ verify-cloud-binary-deb.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Copyright (©) 2003-2026 Teus Benschop.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+echo Verify the bibledit-cloud_n.n.nnn-n_arch.deb
+
+# Exit script on error.
+set -e
+
+DEB=$1
+echo Checking package "$DEB"
+if ! test -f "$DEB"; then
+    echo The file does not exist
+    exit
+fi
+
+# The minimum list of files that should be in the .deb package.
+essential_files=(
+
+usr/bin/bibledit-cloud
+usr/lib/systemd/system/bibledit-cloud.service
+usr/share/man/man1/bibledit-cloud.1.gz
+
+)
+echo Checking the package on ${#essential_files[@]} essential files
+
+dpkg --contents "$DEB" > /tmp/files.txt
+
+for file in "${essential_files[@]}"; do
+  if ! grep -q "$file" /tmp/files.txt; then
+     echo File "$file" does not exist in "$DEB"
+  fi
+done
+
+echo Ready
